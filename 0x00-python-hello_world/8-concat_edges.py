9779a3e72beba569bec8dc10fcd5dd183ff30798
wessam18 <wessamabdelnabi903@gmail.com>
2023-10-30 22:41:44 +0200

    "8"

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str[26:66] + 
-
-print(str[26:66] + str[107:11] + str[0:6])
+str[39:66] + str[107:111] + str[0:6]
+print(str)
