5c6f1b3848d7bd69e007526004def6239e094c5f
wessam18 <wessamabdelnabi903@gmail.com>
2023-11-02 04:55:21 +0200

    "10"

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * check_cycle - check if linked list has a cycle
+ * @list: pointer to head of list.
+ * Return: (0) if no cycle otherwise (1) if has a cycle.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
