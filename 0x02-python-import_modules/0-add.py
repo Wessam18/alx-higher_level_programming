6b1d86864acdc174ad63a5b1b14ca10ded363c73
wessam18 <wessamabdelnabi903@gmail.com>
2023-11-02 16:21:19 +0200

    "0"

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 from add_0 import add
-if __name__ == __main__:
-    def add(a,b):
+if __name__ == "__main__":
+    def add(a, b):
         a = 1
         b = 2
         res = add(a + b)
