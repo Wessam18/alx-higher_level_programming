c00767f1d70040a97b2bce4a40ddf0f20e3f1c58
wessam18 <wessamabdelnabi903@gmail.com>
2023-11-08 11:31:15 +0200

    "0"

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     for row in matrix:
         for col in matrix:
-            res = map(lambda sub: list(map(lambda i: i ** 2, sub)), matrix)
-    print(list(res))
+            res = map(lambda sub: list(map(lambda i: i ** 2, row)), matrix)
+    return list(res)
