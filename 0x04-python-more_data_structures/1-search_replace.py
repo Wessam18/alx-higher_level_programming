7961c2ba40ade6c9641edb9413435d301ff33c37
wessam18 <wessamabdelnabi903@gmail.com>
2023-11-09 00:33:13 +0200

    "1"

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for search in my_list:
-        my_list.replace(search, replace)
-    return my_list
+    new = []
+    for i in my_list:
+        if i == search:
+            new.append(replace)  # Append the replacement value directly
+        else:
+            new.append(i)
+    return new
