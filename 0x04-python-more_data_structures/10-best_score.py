178e5f00a36ccfb71a776935ccf95706e3fbf40a
wessam18 <wessamabdelnabi903@gmail.com>
2023-11-09 03:44:30 +0200

    "10"

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    v = list(a_dictionary.values())
-    k = list(a_dictionary.keys())
-    return k[v.index(max(v))]
+    max = (sorted(a_dictionary.values()))
+    for k in a_dictionary:
+        if a_dictionary[k] == max[-1]:
+            return k
