54aff6ac5da7bc0b7c6adc11dddbb13827ce5748
wessam18 <wessamabdelnabi903@gmail.com>
2023-11-20 22:48:26 +0200

    "2"

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             value = my_list[count]
             if not isinstance(value, int):
                 raise ValueError("Element is not an integer")
-            
+
             print("{:d}".format(value), end="")
             count += 1
     except (ValueError, IndexError):
