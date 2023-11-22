f574424bec49d659868856c8732d0a5075539b46
wessam18 <wessamabdelnabi903@gmail.com>
2023-11-22 04:14:05 +0200

    "5"

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """ define square class"""
+
+    def __init__(self, size=0):
+        """instance constructor"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """area of the square"""
+
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            return ()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
