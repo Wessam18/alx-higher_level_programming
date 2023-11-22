c71bbce814284c1d6549deb4913c10c144096069
wessam18 <wessamabdelnabi903@gmail.com>
2023-11-22 02:35:34 +0200

    "1"

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """ define square class"""
+
+    def __init__(self, size):
+        """instance constructor"""
+
+        self.__size = size
