95601bae60f195bc3ea3505e7abef1f1299b7fcf
wessam18 <wessamabdelnabi903@gmail.com>
2023-12-03 05:53:59 +0200

    "5"

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
+"""
+    A function that print text.
+Args:
+        text: the text will be print
+
+Raises:
+        TypeError: If text is not a string.
+"""
+
+
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each '.', '?', and ':' characters.
-
-    :param text: The input text.
-    :raises TypeError: If text is not a string.
     """
-    # Check if text is a string
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    # Initialize an empty string to store the result
     result = ""
 
-    # Iterate through each character in the input text
     for char in text:
-        # Add the character to the result string
         result += char
-
-        # Check if the character is '.', '?', or ':'
         if char in ['.', '?', ':']:
-            # Add two new lines after the character
             result += '\n\n'
 
-    # Print the result string with leading and trailing whitespaces removed
     print(result.strip())
