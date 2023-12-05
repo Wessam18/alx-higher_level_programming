bf1eb8f2ac70bc10eae4fcf7dce123600cdc8802
wessam18 <wessamabdelnabi903@gmail.com>
2023-12-05 03:38:58 +0200

    "6"

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    write an empty class
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """fun to determine the area"""
+        raise Exception("area() is not implemented")
