cbe139edde0ddf46da4b51cbc0263bc5ab97979e
wessam18 <wessamabdelnabi903@gmail.com>
2023-12-05 04:51:45 +0200

    "11"

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    write a class inherit from Rectangle.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        """ """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """function to determine the area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ """
+        return "[Square] {}/{}".format(
+                self.__size, self.__size)
