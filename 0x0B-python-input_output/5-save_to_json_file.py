d4f9ee00a31b5b9ab2a8be54e34a1c3f49876295
wessam18 <wessamabdelnabi903@gmail.com>
2023-12-06 04:00:03 +0200

    "3 to 5"

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Write a function that returns an object
+(Python data structure) represented by a JSON string
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function that returns the deseialize data"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return json.dumps(my_obj)
