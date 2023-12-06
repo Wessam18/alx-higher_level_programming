ca17ef99f5c72fe5d1181960b8dee6fb9deb9c51
wessam18 <wessamabdelnabi903@gmail.com>
2023-12-06 04:50:40 +0200

    "100"

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,10 +12,8 @@ def append_after(filename="", search_string="", new_string=""):
         new_string (str): The string to append
         after the lines containing the search string.
     """
-    with open(filename, 'r') as file:
+    with open(filename, 'r+') as file:
         lines = file.readlines()
-
-    with open(filename, 'w') as file:
         for line in lines:
             file.write(line)
             if search_string in line:
