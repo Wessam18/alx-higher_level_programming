09e12ca3795f9a00b26b38f627e3b1f66e5f9725
wessam18 <wessamabdelnabi903@gmail.com>
2023-12-06 04:09:59 +0200

    "6"

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,5 +9,5 @@ import json
 
 def load_from_json_file(filename):
     """function that returns the deseialize data"""
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         return json.load(filename)
