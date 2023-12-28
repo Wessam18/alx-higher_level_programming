9f02314f72bc44b5bb8367d776aa12bf0fa1e5bd
wessam18 <wessamabdelnabi903@gmail.com>
2023-12-28 12:15:32 +0200

    "init"

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,17 +6,9 @@ class Square:
     """ define square class"""
 
     def __init__(self, size=0, position=(0, 0)):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        elif not isinstance(position, tuple) or len(position) != 2 or \
-            not isinstance(position[0], int) or position[0] < 0 or \
-                not isinstance(position[1], int) or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__size = size
-            self.__position = position
+        """init method"""
+        self.__size = size
+        self.__position = position
 
     def area(self):
         """area of the square"""
