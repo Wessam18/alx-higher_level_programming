958e2a650b369b41bee51e58475ab8bca908279c
wessam18 <wessamabdelnabi903@gmail.com>
2023-12-28 14:26:01 +0200

    "init"

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,8 +52,9 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for i in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+        else:
+            if self.__position[1] > 0:
+                print()
+            for i in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
