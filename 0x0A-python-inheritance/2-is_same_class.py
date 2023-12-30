9d69fc785215408320ed2e91743f45e2abdc94c1
wessam18 <wessamabdelnabi903@gmail.com>
2023-12-30 20:35:26 +0200

    "2"

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,10 +3,10 @@
     A function determine the object is
     exactly an instance of the specified class
 Args:
-        obj: the object to test
-        a_class: the parent
+	obj: the object to test
+	a_class: the parent
 Return:
-        true or false
+	true or false
 """
 
 
