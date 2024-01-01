8cb5b1473922b79d54beae22ed2047c7337ce315
wessam18 <wessamabdelnabi903@gmail.com>
2024-01-01 12:01:53 +0200

    "10"

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,8 +9,8 @@ class Square(Rectangle):
     """Square class"""
     def __init__(self, size):
         """ """
-        super().__init__(size, size)
         super().integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
