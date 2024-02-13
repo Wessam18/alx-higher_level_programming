24f0d151058d85ee1d6c92dea091d5d50afe137c
wessam18 <wessamabdelnabi903@gmail.com>
2024-02-13 02:29:42 +0200

    task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const arg = process.argv;
+const x = arg[2];
+
+if (x) {
+  for (let i = 1; i <= x; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
