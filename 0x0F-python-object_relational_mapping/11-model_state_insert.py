0d0281ad502e8ae02304519be7aad5618156b6e9
wessam18 <wessamabdelnabi903@gmail.com>
2024-02-18 06:22:36 +0200

    task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -12,12 +12,12 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
-    session = sessionmaker(bind=engine)
-    session = session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    new_state = State(name = "Louisiana")
+    new_state = State(name='Louisiana')
 
     session.add(new_state)
     session.commit()
 
-    print("{}".format(State.id))
+    print("{}".format(new_state.id))
