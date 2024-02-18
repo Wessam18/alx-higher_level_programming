47aa5024afdf3a2d41315f622f5f3f8bb4d48f3a
wessam18 <wessamabdelnabi903@gmail.com>
2024-02-18 06:53:50 +0200

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""import modules"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """create engine"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(State.id == 2).first()
+    result.name("New Mexico")
+    session.commit()
+    
