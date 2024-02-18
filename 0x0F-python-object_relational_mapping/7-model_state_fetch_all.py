448f622ed6bf94252703ab2c39a0cb8be67f09d2
wessam18 <wessamabdelnabi903@gmail.com>
2024-02-18 05:39:41 +0200

    fix 8

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,14 +3,13 @@
 
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """create engine"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1],
-                            sys.argv[2], sys.argv[3]),
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                             pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,14 +3,13 @@
 
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """create engine"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1],
-                            sys.argv[2], sys.argv[3]),
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                             pool_pre_ping=True)
 
     session = sessionmaker(bind=engine)
@@ -18,7 +17,7 @@ if __name__ == "__main__":
 
     states = session.query(State).limit(1).first()
 
-    for i in states:
+    if (states):
         print("{}: {}".format(State.id, State.name))
     else:
         print("Nothing")
