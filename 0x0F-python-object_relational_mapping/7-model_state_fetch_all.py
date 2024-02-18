34807d281f1d3c943c0f97b92332192a3190ffc5
wessam18 <wessamabdelnabi903@gmail.com>
2024-02-18 04:21:11 +0200

    task7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""import modules"""
 
 import sys
 from model_state import Base, State
@@ -6,6 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    """create engine"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                         .format(sys.argv[1],
                             sys.argv[2], sys.argv[3]),
