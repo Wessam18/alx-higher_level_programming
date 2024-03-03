d30f1c79e66983a4347c198bd2baf19795593fad
wessam18 <wessamabdelnabi903@gmail.com>
2024-03-03 04:37:04 +0200

    edit 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a request to that URL
-curl -s -w "$1"
+curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request
-curl -sL "$1"
+curl -sX "$1"
