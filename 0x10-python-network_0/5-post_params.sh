68fd4a39993b93a3c19be8c1aad2c6d7bd57a992
wessam18 <wessamabdelnabi903@gmail.com>
2024-03-03 05:20:29 +0200

    edit 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL,
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
