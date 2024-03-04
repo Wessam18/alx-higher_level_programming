554dfcc8355f88a0c4b9f943a750148b4a364f05
wessam18 <wessamabdelnabi903@gmail.com>
2024-03-04 05:51:03 +0200

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""import modules"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    """ """
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        x = response.text
+        print(x)
+
+    
