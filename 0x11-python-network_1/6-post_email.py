02effc051ddab680c6e1dff52d22f6367873298f
wessam18 <wessamabdelnabi903@gmail.com>
2024-03-04 05:41:22 +0200

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""import modules"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    """ """
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    query = {'email': email}
+
+    response = requests.post(url, data=query)
+    x = response.read()
+    print(x)
