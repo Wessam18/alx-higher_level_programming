5dea2b663cd7ac223dee62e79ab8bfc851e64b33
wessam18 <wessamabdelnabi903@gmail.com>
2024-03-04 04:32:34 +0200

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    query = parse.urlencode({'email' : email})
-    query = query.encode('utf-8')
+    query = parse.urlencode({'email': email}).encode('utf-8')
 
     with request.urlopen(url, query) as response:
         x = response.read().decode('utf-8')
