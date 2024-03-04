60340ad3f0c2f6b95d7693cdcf9e9d7cbcd0fd77
wessam18 <wessamabdelnabi903@gmail.com>
2024-03-04 03:46:49 +0200

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,5 +7,5 @@ import sys
 if __name__ == "__main__":
     """ """
     with request.urlopen(sys.argv[1]) as response:
-        x = response.headers.get('X-Request-Id')
+        x = response.headers['X-Request-Id']
         print(x)
---
+++ 0x11-python-network_1/wess.py
@@ -0,0 +1,4 @@
+from urllib import request
+import http.client
+
+print(dir(http.client.HTTPResponse))
