3f33c1b59fc54878b7bfd9fa96b2acbe62a6e7fe
wessam18 <wessamabdelnabi903@gmail.com>
2024-04-08 17:50:46 +0200

    1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const filepath = process.argv[2];
 
-fs.readFile(process.argv[2], 'utf8', (error, Data) => {
+fs.readFile(filepath, 'utf8', (error, Data) => {
   if (error) {
     console.error('Error:', error);
     return;
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const data = process.argv[3];
 
-try {
-  fs.writeFileSync(filePath, data, 'utf-8');
-} catch (err) {
-  console.log(err);
-}
+fs.writeFile(filePath, data, 'utf8', (error) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+});
