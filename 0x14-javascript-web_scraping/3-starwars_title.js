892b7ebbf42851433d3c5576b10c019f68907450
wessam18 <wessamabdelnabi903@gmail.com>
2024-04-08 23:20:28 +0200

    3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,11 @@ const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 
-request.get(url, (err, res, body) => {
+
+request.get(url, (error, Response, body) => {
+  if (error) {
+    return;
+  }
   const movie = JSON.parse(body);
-  console.log(err || movie.title);
+  console.log(movie.title);
 });
