67f87e1b5255a41181274ee2b397b9eeb2c0fd48
wessam18 <wessamabdelnabi903@gmail.com>
2024-04-09 06:18:03 +0200

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,9 +6,14 @@ const antiId = '18';
 
 request.get(url, (error, Response, body) => {
   if (error) {
+    console.error(error);
     return;
   }
   const movie = JSON.parse(body).results;
+  if (!movie) {
+    console.log(0);
+    return;
+  }
   const a = movie.filter(function (el) {
     return el.characters.includes(`https://swapi-api.alx-tools.com/api/people/${antiId}/`);
   });
