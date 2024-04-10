4670efae95a2530678f5f479e929e2340a4c4577
wessam18 <wessamabdelnabi903@gmail.com>
2024-04-10 05:15:09 +0200

    0

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,16 +2,18 @@
 
 const request = require('request');
 const fs = require('fs');
+const { json } = require('stream/consumers');
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 const writeStream = fs.createWriteStream(filePath);
 
-request(url, (err, resp, data) => {
+request(url, (err, resp, body) => {
   if (err) {
     console.log(err);
   }
+  const x = JSON.parse(body);
 
-  writeStream.write(data);
+  writeStream.write(x.data);
   writeStream.end();
 });
